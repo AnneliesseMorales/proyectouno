d92d34d6f0731ae6bb07cb277e3807a67090a999
AnneliesseMorales <anelism83@gmail.com>
2022-01-01 17:52:16 -0500

    commit 1

---
+++ src/components/NavComponents.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const NavComponent =()=>{
+    return (
+        <div className ="navContainer">
+            <div>
+                <p>Brand</p>
+            </div>
+            <div className = "nav-menu">
+                <p>Home</p>
+                <p>Productos</p>
+                <p>Nosotros</p>
+                <p>Contacto</p>
+            </div>
+            <div>
+                <p>Search</p>
+            </div>
+        </div>
+    )
+}
