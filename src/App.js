928890acb92b4d8158615bca9eb017a4ae305685
AnneliesseMorales <anelism83@gmail.com>
2022-01-23 15:44:00 -0500

    codigo de https://dev.to/stephanieopala/simple-navigation-bar-in-react-js-4d5m

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-
+import React from "react";
 import './App.css';
-import Home from './pages/Home';
-import Products from './pages/Products';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './pages';
+import About from './pages/about';
+import Contact from './pages/contact';
+import SignUp from './pages/signup';
+import SignIn from './pages/signin';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Switch>
-      <Route exact path="/">
-        <Home/>
-      </Route>
-      <Route exact path="/Products">
-        <Products/>
-      </Route>
-    </Switch>
-    </BrowserRouter>
-     
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/sign-up" component={SignUp} />
+        <Route path="/Products" component={Products} />
+      </Switch>
+    </Router>
   );
 }
+
 export default App;
